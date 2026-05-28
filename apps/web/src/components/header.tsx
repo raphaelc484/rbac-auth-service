1fff3890735a52d83cf56c20548f7852ca9ad18d
raphaelc484 <raphaelc484@hotmail.com>
2026-05-28 11:55:14 -0300

    front - validando permissões

--- apps/web/src/components/header.tsx
+++ apps/web/src/components/header.tsx
@@ -2,11 +2,14 @@ import { Slash } from 'lucide-react'
 import Image from 'next/image'
 
 import rocketseatIcon from '@/assets/rocketseat-icon.svg'
+import { ability } from '@/auth/auth'
 
 import { OrganizationSwitcher } from './organization-switcher'
 import { ProfileButton } from './profile-button'
 
-export function Header() {
+export async function Header() {
+  const permissions = await ability()
+
   return (
     <div className="mx-auto flex max-w-300 items-center justify-between">
       <div className="flex items-center gap-3">
@@ -14,6 +17,8 @@ export function Header() {
 
         <Slash className="text-border size-3 -rotate-24" />
         <OrganizationSwitcher />
+
+        {permissions?.can('get', 'Project') && <p>Projetos</p>}
       </div>
 
       <div className="flex items-center gap-4">
