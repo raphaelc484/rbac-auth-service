ee7ac3f47e5a8531faec78c3f9effead8f7e7de7
raphaelc484 <raphaelc484@hotmail.com>
2026-06-01 13:44:56 -0300

    front - pag. criar organização

--- apps/web/src/components/header.tsx
+++ apps/web/src/components/header.tsx
@@ -13,7 +13,7 @@ export async function Header() {
   const permissions = await ability()
 
   return (
-    <div className="mx-auto flex max-w-300 items-center justify-between">
+    <div className="mx-auto flex max-w-300 items-center justify-between border-b pb-2">
       <div className="flex items-center gap-3">
         <Image src={rocketseatIcon} className="size-6" alt="Rocketseat" />
 
